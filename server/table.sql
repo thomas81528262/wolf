56c6a0d1063feb33b9350d33588ce37e933d412c
thomas <thomas81528262@hotmail.com>
2020-05-01 14:48:51 -0700

    add sheme

--- server/table.sql
+++ server/table.sql
@@ -15,3 +15,18 @@ CREATE TABLE "role" (
 	"number" int4 NULL
 );
 
+CREATE TABLE template_role (
+	roleid int4 NOT NULL,
+	"number" int4 NOT NULL,
+	"name" varchar NOT NULL,
+	darkpriority int4 NULL,
+	CONSTRAINT template_role_pk PRIMARY KEY (roleid, name),
+	CONSTRAINT template_role_fk FOREIGN KEY (name) REFERENCES template_header(name) ON DELETE CASCADE
+);
+
+CREATE TABLE template_header (
+	"name" varchar NOT NULL,
+	description varchar NULL,
+	isenabled bool NULL,
+	CONSTRAINT template_header_pk PRIMARY KEY (name)
+);
