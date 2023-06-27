913dbf511f3a46d26c83a81c893afe0ed074d1b2
thomas yeh <thomasyeh@thomasdembp-2.lan>
2023-06-26 21:35:23 -0700

    implement ormDb enableTemplate

--- server/src/ormDb.ts
+++ server/src/ormDb.ts
@@ -8,9 +8,24 @@ export default class Db {
         this.#client = new PrismaClient()
     }
 
+    async enableTemplate({ name }:{name:string}) {
+      await this.#client.$transaction(async (tx)=>{
+
+        const disalbedTemplate = await tx.template_header.updateMany({data:{isenabled:false},where:{}});
+
+        if (disalbedTemplate.count !==1) {
+          console.warn('DB: Enabled template number error!')
+        }
+        const enabledTemplate = await tx.template_header.updateMany({data:{isenabled:true}, where:{name}})
+        if (enabledTemplate.count !==1) {
+          throw new Error(`DB: enalbe template ${name} fail!`)
+        }
+      })
+    }
+
     async getAllTemplate() {
         try {
-          const result = await this.#client.template_header.findMany();
+          const result = await this.#client.template_header.findMany({orderBy:{name:'asc'}});
           return result;
         } catch (err) {
           if (err instanceof Error) {
@@ -30,8 +45,9 @@ export default class Db {
 const db = new Db();
 
 async function main() {
-  
+  await db.enableTemplate({name:'019. 混沌之魔審判官靈鹿'})
   const result = await db.getAllTemplate();
+  
   console.log(result);
 }
 
