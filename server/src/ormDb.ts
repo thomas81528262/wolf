cf1951c0550072f64e663a8eefa229043309235c
thomas yeh <thomasyeh@thomasdembp.lan>
2023-06-29 21:37:10 -0700

    implement  getTemplate getTemplateRole use select to select the column change schema.prisma to use left join

--- server/src/ormDb.ts
+++ server/src/ormDb.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient,  Prisma} from '@prisma/client'
 
 
 
@@ -11,10 +11,10 @@ export default class Db {
     async enableTemplate({ name }:{name:string}) {
       await this.#client.$transaction(async (tx)=>{
 
-        const disalbedTemplate = await tx.template_header.updateMany({data:{isenabled:false},where:{}});
+        const disalbedTemplate = await tx.template_header.updateMany({data:{isenabled:false},where:{isenabled:true}});
 
         if (disalbedTemplate.count !==1) {
-          console.warn('DB: Enabled template number error!')
+          console.warn('DB: Enabled template number error!', disalbedTemplate.count)
         }
         const enabledTemplate = await tx.template_header.updateMany({data:{isenabled:true}, where:{name}})
         if (enabledTemplate.count !==1) {
@@ -23,9 +23,21 @@ export default class Db {
       })
     }
 
-    async getAllTemplate() {
+    async updateTemplateRolePriority({ ids, name }:{ids:number[], name:string}) {
+     
+        
+        await this.#client.$transaction(async (tx)=>{
+
+          for (let i = 0; i < ids.length; i += 1) {
+            const roleId = ids[i];
+            await tx.template_role.update({data:{darkpriority:i}, where:{roleId_name:{roleId, name}}})
+          }
+        });
+    }
+
+    async getTemplate({select, where}:{select:Prisma.template_headerSelectScalar, where:Prisma.template_headerWhereInput}) {
         try {
-          const result = await this.#client.template_header.findMany({orderBy:{name:'asc'}});
+          const result = await this.#client.template_header.findMany({orderBy:{name:'asc'}, select, where});
           return result;
         } catch (err) {
           if (err instanceof Error) {
@@ -37,6 +49,23 @@ export default class Db {
         }
     }
 
+    async getTemplateRole({select, where}:{select:Prisma.template_roleSelectScalar, where:Prisma.template_roleWhereInput }){
+
+      try {
+        const result = await this.#client.template_role.findMany({select:{role:{select:{name:true}},...select}, where});
+        return result;
+      } catch (err) {
+        if (err instanceof Error) {
+          console.error(err.message);
+        } else {
+          console.log('Unexpected error', err);
+        }
+        return [];
+      }
+
+
+    }
+
     async $disconnect() {
         await this.#client.$disconnect();
     }
@@ -46,9 +75,9 @@ const db = new Db();
 
 async function main() {
   await db.enableTemplate({name:'019. 混沌之魔審判官靈鹿'})
-  const result = await db.getAllTemplate();
-  
-  console.log(result);
+  const result = await db.getTemplate({select:{name:true, description:true}, where:{name:'013. 血月獵魔人'}});
+  const re = await db.getTemplateRole({select:{ number:true, roleId:true}, where:{name:'013. 血月獵魔人'}})
+  console.log(result, re);
 }
 
 main()
